3b8d3c6d0043ef4137d75c640138f5ad080bb13c
Ruchi Dadwani <ruchidadwani@gmail.com>
2026-03-23 21:13:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,8 +5,8 @@ class Solution {
         int el=0;
         for(int i=0; i<nums.length; i++){
             if(count==0){
-                count=1;
                 el=nums[i];
+                count=1;
             }
             else if(nums[i]==el){
                 count++;
