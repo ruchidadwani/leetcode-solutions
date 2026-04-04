58ad34c5b42049b08a24e56bbeb29a93cad16a7c
Ruchi Dadwani <ruchidadwani@gmail.com>
2026-04-05 00:16:34 +0530

    Time: 7 ms (22.81%), Space: 43.3 MB (48%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if (n + m != s3.length()) return false;
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        // First column (only s1)
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+        }
+
+        // First row (only s2)
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = dp[0][j - 1] && (s2.charAt(j - 1) == s3.charAt(j - 1));
+        }
+
+        // Fill DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                char c = s3.charAt(i + j - 1);
+
+                if (s1.charAt(i - 1) == c) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+
+                if (s2.charAt(j - 1) == c) {
+                    dp[i][j] = dp[i][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
