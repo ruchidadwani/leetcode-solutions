afbf615c4ae7f3c313f10545ddc7f1645ce6f947
Ruchi Dadwani <ruchidadwani@gmail.com>
2026-05-31 10:33:11 +0530

    Time: 3 ms (73.82%), Space: 50.5 MB (10.38%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,20 +3,23 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        for (int i = 1; i < m; i++) {
-            grid[i][0] += grid[i - 1][0];
-        }
+        int[] dp = new int[n];
 
+        dp[0] = grid[0][0];
+
+        // First row
         for (int j = 1; j < n; j++) {
-            grid[0][j] += grid[0][j - 1];
+            dp[j] = dp[j - 1] + grid[0][j];
         }
 
         for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+
             for (int j = 1; j < n; j++) {
-                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
             }
         }
 
-        return grid[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
