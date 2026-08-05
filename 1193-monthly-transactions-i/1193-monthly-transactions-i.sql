37157cd47029069ce819965c8a4cfecac4d77ca8
Ruchi Dadwani <ruchidadwani@gmail.com>
2026-08-05 21:08:02 +0530

    Time: 1171 ms (5%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -7,5 +7,5 @@ SUM(state = 'approved') AS approved_count,
 SUM(amount) AS trans_total_amount, 
 SUM(CASE WHEN state='approved' THEN amount ELSE 0 END)AS approved_total_amount
 FROM Transactions
-GROUP BY YEAR(trans_date), MONTH(trans_date), country
+GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country
 ;
